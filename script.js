84ef72468643825c7556e914a6f83edec23a48ae
RoshanSyangtanTamang <rossonsyangtan444@gmail.com>
2025-04-03 20:55:22 +0545

    Login Page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".login-popup");
+const btnClose = document.querySelector(".close");
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -8,4 +10,12 @@ registerLink.addEventListener('click', () => {
 
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+btnClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
 });
